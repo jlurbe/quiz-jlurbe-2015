c833a3a4d35c0fd7523ca8f7c8bdc93351faaa52
jlurbe <juanjo.lurbe@gmail.com>
2015-06-30 17:45:34 +0200

    Incluido buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,7 +16,8 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req,res) {
-	models.Quiz.findAll().then(function(quizes){
+	var search = req.query.search==undefined?"":req.query.search;
+	models.Quiz.findAll({where: ["pregunta like ?", '%'+search+'%']}).then(function(quizes){
 		res.render('quizes/index.ejs', {quizes: quizes});
 	}).catch(function(error) {next(error);});
 }

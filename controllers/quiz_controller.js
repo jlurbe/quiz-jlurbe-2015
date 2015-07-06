c8fd7afbf937b045b81f82960679ead615a29447
jlurbe <juanjo.lurbe@gmail.com>
2015-07-06 23:28:11 +0200

    Corregido error en la página del autor

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -38,7 +38,7 @@ exports.answer = function(req,res) {
 
 // GET /quizes/author
 exports.author = function(req, res) {
-	res.render('quizes/author');
+	res.render('quizes/author',{errors:[]});
 };
 
 // GET /quizes/new
